bc92b02f4f9d14f21a24cbdbacd649ef78afbce6
Parthiban <parthiban51199@gmail.com>
2025-01-12 11:05:24 +0530

    Test Commit
    Demo

--- src/main/webapp/script/UserJavaScript.js
+++ src/main/webapp/script/UserJavaScript.js
@@ -9,26 +9,26 @@ function showPassword() {
 }
 
 //Display User Address
-function showAddress(divId, element,pincode) {
-	
+function showAddress(divId, element, pincode) {
+
 	let delivery = document.getElementById(divId);
 	let pickup = document.getElementById('pickup-total');
 	let error = document.getElementById('addressError');
 	let confirm = document.getElementById('confirmOrder');
-	if(element.value === 'delivery'){
-		if(checkPincode(pincode)){
+	if (element.value === 'delivery') {
+		if (checkPincode(pincode)) {
 			delivery.style.display = 'block';
 			pickup.style.display = 'none';
 			error.style.display = 'none';
 		}
-		else{
+		else {
 			delivery.style.display = 'block';
 			pickup.style.display = 'none';
 			error.style.display = 'block';
 			confirm.disabled = true;
 		}
 	}
-	else{
+	else {
 		delivery.style.display = 'none';
 		pickup.style.display = 'block';
 		error.style.display = 'none';
@@ -37,32 +37,32 @@ function showAddress(divId, element,pincode) {
 }
 
 //check pincode
-function checkPincode(pincode){
+function checkPincode(pincode) {
 	const code = pincode;
-	if(code>=600001 && code<=602107) {
+	if (code >= 600001 && code <= 602107) {
 		console.log("true");
 		return true;
 	}
-	else{
+	else {
 		console.log("false");
 		return false;
 	}
 }
 //Edit
 function myFunction() {
-		let name = document.getElementById("edit-name");
-		let phoneNumber = document.getElementById("edit-phoneNumber");
-		let address = document.getElementById("edit-addresss");
-		let password = document.getElementById("edit-password");
-		let submit = document.getElementById("edit-submit");
-		if (submit.disabled === true) {
-			name.disabled = false;
-			address.disabled = false;
-			phoneNumber.disabled = false;
-			password.disabled = false;
-			submit.disabled = false;
-		}
+	let name = document.getElementById("edit-name");
+	let phoneNumber = document.getElementById("edit-phoneNumber");
+	let address = document.getElementById("edit-addresss");
+	let password = document.getElementById("edit-password");
+	let submit = document.getElementById("edit-submit");
+	if (submit.disabled === true) {
+		name.disabled = false;
+		address.disabled = false;
+		phoneNumber.disabled = false;
+		password.disabled = false;
+		submit.disabled = false;
 	}
+}
 
 //Order Items*
 function showOrderItem() {
@@ -75,4 +75,4 @@ function showOrderItem() {
 		x.style.display = "none";
 		y.style.width = "96.6%";
 	}
-}
+} 
